a4aab9e1d4d3406fadf1ecd966539c4cd98c99af
Ken <ken2810077@gmail.com>
2026-06-13 13:55:06 +0800

    新增矩陣的加減法 2026-06-13

--- Matrix.py
+++ Matrix.py
@@ -1,9 +1,18 @@
+from unittest import result
+
+from _testcapi import error
+
+
 class Matrix:
     def __init__(self, data):
         self.data = data
 
         self.row = len(data)
         self.column = len(data[0])
+
+        for row in data:
+            assert len(row) == self.column, "Matrix syntax error"
+
 
 
     def __repr__(self):
@@ -15,3 +24,31 @@ class Matrix:
             reslut += "\n"
 
         return reslut
+
+    def __add__(self, other):
+
+        assert self.row == other.row and self.column == other.column, "Matrix syntax error"
+
+        result = []
+
+        for r in range(self.row):
+            row = []
+            for c in range(self.column):
+                row.append(self.data[r][c] + other.data[r][c])
+            result.append(row)
+
+        return Matrix(result)
+
+    def __sub__(self, other):
+
+        assert self.row == other.row and self.column == other.column, "Matrix syntax error"
+
+        result = []
+
+        for r in range(self.row):
+            row = []
+            for c in range(self.column):
+                row.append(self.data[r][c] - other.data[r][c])
+            result.append(row)
+
+        return Matrix(result)
--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from Matrix import Matrix
 
 if __name__ == "__main__":
     A = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    B = Matrix([[10, 11, 12], [13, 14, 15], [16, 17, 18]])
 
-    print(A)
+    C = A + B
+
+    print(C)
