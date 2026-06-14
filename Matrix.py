6fd151eb32a1ff8c14c1b0930cf4d6c036383593
Ken <ken2810077@gmail.com>
2026-06-14 20:25:21 +0800

    新增矩陣的乘法 2026-06-14

--- Matrix.py
+++ Matrix.py
@@ -8,10 +8,29 @@ class Matrix:
 
     def __repr__(self):
 
-        reslut = ""
+        result = ""
 
         for row in self.data:
-            reslut += " ".join(map(str, row))
-            reslut += "\n"
+            result += " ".join(map(str, row))
+            result += "\n"
 
-        return reslut
+        return result
+
+    def __mul__(self, other):
+
+        assert isinstance(other, Matrix), "the variable is not a Matrix"
+
+        assert self.column == other.row, "the number of columns of the former matrix is not equal to the number of rows of the latter one"
+
+
+        result = []
+        for rows in range(self.row):
+            row = []
+
+            for col in range(other.column):
+                element = 0
+                for c in range(self.column):
+                    element += (self.data[rows][c] ) * (other.data[c][col])
+                row.append(element)
+            result.append(row)
+        return Matrix(result)
